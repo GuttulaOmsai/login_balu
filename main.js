153c2e156e26f0d72fa0275a6d42a9ac450de173
GuttulaOmsai <125951811+GuttulaOmsai@users.noreply.github.com>
2024-04-03 10:48:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+const root = document.documentElement;
+const eye = document.getElementById('eyeball');
+const beam = document.getElementById('beam');
+const passwordInput = document.getElementById('password');
+
+root.addEventListener('mousemove', (e) => {
+  let rect = beam.getBoundingClientRect();
+  let mouseX = rect.right + (rect.width / 2); 
+  let mouseY = rect.top + (rect.height / 2);
+  let rad = Math.atan2(mouseX - e.pageX, mouseY - e.pageY);
+  let degrees = (rad * (20 / Math.PI) * -1) - 350;
+
+  root.style.setProperty('--beamDegrees', `${degrees}deg`);
+});
+
+eye.addEventListener('click', e => {
+  e.preventDefault();
+  document.body.classList.toggle('show-password');
+  passwordInput.type = passwordInput.type === 'password' ? 'text' : 'password'
+  passwordInput.focus();
+});
+
+  document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('submit').addEventListener('click', function () {
+      var username = document.getElementById('username').value;
+      var password = document.getElementById('password').value;
+
+      if (username.trim() === '') {
+        alert('Please enter your username');
+        return false; // Prevent form submission
+      }
+
+      if (password.trim() === '') {
+        alert('Please enter your password');
+        return false; // Prevent form submission
+      }
+
+      // If both fields are filled, open 'j.html' in a new window
+      window.open('j.html', '_blank');
+      return false; // Prevent default form submission
+    });
+  });
+
